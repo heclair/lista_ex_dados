2795156fff4c1acba7a60940dcd8cf9b06c67a2b
heclair <heclair@hotmail.com>
2023-08-17 22:07:56 -0300

    aula

--- src/ex05.ts
+++ src/ex05.ts
@@ -1,24 +1,31 @@
-//P.1.5. Escreva um programa, em TypeScript, que solicite que o usuário digite duas palavras e diga qual delas aparece antes da outra no dicionário. O programa não deve solicitar nenhuma 
+//. Escreva um programa, em TypeScript, que solicite que o usuário digite duas palavras e diga qual delas aparece antes da outra no dicionário
 
-class Dicionario {
-    c: string;
-    d: string;
-    
-    constructor(c: string, d: string) {
-      this.c = c;
-      this.d = d;
-    }
+class Dicionario1 {
+  e: string;
+  f: string;
 
-    print(){
-        if (this.c<this.d){
-            console.log(`Palavra ${this.c} é anterior a ${this.d}`);
-        }
-        else{
-            console.log(`Palavra ${this.d} é anterior a ${this.c}`);
-        }
+  constructor(e: string, f: string) {
+    this.e = e;
+    this.f = f;
+  }
+
+  comparar() {
+    for (let i = 0; i < this.e.length && i < this.f.length; i++) {
+      const a = this.e.charCodeAt(i);
+      const b = this.f.charCodeAt(i);
     }
-    
+  }
+
+  print() {
+    if (this.e < this.f) {
+      console.log(`Palavra ${this.e} é anterior a ${this.f}`);
+    } else {
+      console.log(`Palavra ${this.f} é anterior a ${this.e}`);
+    }
+  }
 }
 
-const teste3 = new Dicionario("amor","arroz");
-teste3.print();
+const teste4 = new Dicionario1("amor", "arroz");
+teste4.print();
+
+//O programa não deve solicitar nenhuma informação adicional por parte do usuário e supõe que as palavras são escritas somente com caracteres de ‘a’ a ‘z’. Para esse caso, especifique, também, o algoritmo em pseudocódigo, conforme notação explicada em sala de aula. Utilize o método de string charCodeAt().
