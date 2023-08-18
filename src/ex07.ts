45cc7e3f2cb5c5e483dd1de9798f2cf08a1aec74
heclair <heclair@hotmail.com>
2023-08-17 22:16:35 -0300

    aula

---
+++ src/ex07.ts
@@ -0,0 +1,2 @@
+//P.1.7. Escreva um programa que exiba um menu contendo 3 alternativas: “1. Dúvidas”, “2.Reclamações”, “3.Sair”. O usuário deve digitar a palavra correspondente à opção do menu e, dependendo da opção, deve ser fornecida uma orientação ao usuário. Exemplo: Caso o usuário digite “Dúvidas” exiba: “Suas dúvidas devem ser encaminhadas para o email duvidas@email.com”. Esse programa deve ser escrito em TypeScript e deve fazer uso do bloco switch. 
+
